e938f2f49b5cb0142ce3a8ffad306f4863f689a1
Miralem.Ba <miralembanovi@gmail.com>
2024-03-20 13:15:19 +0100

    const msgs edited

--- example_project/example_project/src/index.js
+++ example_project/example_project/src/index.js
@@ -2,20 +2,15 @@ import hh from "hyperscript-helpers";
 import { h, diff, patch } from "virtual-dom";
 import createElement from "virtual-dom/create-element";
 
-// allows using html tags as functions in javascript
-const { div, button, p, h1 } = hh(h);
+const { div, button, input, p } = hh(h);
 
-// A combination of Tailwind classes which represent a (more or less nice) button style
 const btnStyle = "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded";
 
-// Messages which can be used to update the model
 const MSGS = {
-  UPDATE_MODEL: "UPDATE_MODEL",
-  UPDATE_RANDOM_NUMBER: "UPDATE_RANDOM_NUMBER",
-  // ... ℹ️ additional messages
+  ADD_LOCATION: 'ADD_LOCATION',
+  DELETE_LOCATION: 'DELETE_LOCATION',
 };
 
-// View function which represents the UI as HTML-tag functions
 function view(dispatch, model) {
   return div({ className: "flex flex-col gap-4 items-center" }, [
     h1({ className: "text-2xl" }, `My Title`),
@@ -23,11 +18,10 @@ function view(dispatch, model) {
     p({ className: "text-2xl" }, `Time: ${model.currentTime}`),
     button({ className: btnStyle, onclick: () => dispatch(MSGS.UPDATE_RANDOM_NUMBER) }, "Update Random Number"),
     p({ className: "text-2xl" }, `Random Number: ${model.randomNumber}`),
-    // ... ℹ️ additional elements
   ]);
 }
 
-// Update function which takes a message and a model and returns a new/updated model
+
 function update(msg, model) {
   switch (msg) {
     case MSGS.UPDATE_MODEL:
@@ -40,7 +34,7 @@ function update(msg, model) {
   }
 }
 
-// ⚠️ Impure code below (not avoidable but controllable)
+
 function app(initModel, update, view, node) {
   let model = initModel;
   let currentView = view(dispatch, model);
@@ -55,14 +49,12 @@ function app(initModel, update, view, node) {
   }
 }
 
-// The initial model when the app starts
 const initModel = {
   currentTime: new Date().toLocaleTimeString(),
   randomNumber: 1,
 };
 
-// The root node of the app (the div with id="app" in index.html)
+
 const rootNode = document.getElementById("app");
 
-// Start the app
 app(initModel, update, view, rootNode);
